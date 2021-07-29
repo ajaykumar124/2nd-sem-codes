0fef515e1913faba3335df68f85a1f903fc40be9
Ajay kumar <ajaykumarrajput345@gmail.com>
2021-07-29 21:03:50 +0530

    Add files via upload

---
+++ 6call_in_derived_class.cpp
@@ -0,0 +1,26 @@
+//Write a program to demonstrate constructor call in the derived class.
+
+#include<iostream>
+using namespace std;
+class Parent
+{
+public:
+Parent()
+{
+cout<< "Inside base class"<<endl;
+}
+};
+class Child: public Parent
+{
+public:
+Child()
+{
+cout<< "Inside sub class"<<endl;
+}
+};
+int main()
+{
+ 
+Child obj; return 0;
+}
+
